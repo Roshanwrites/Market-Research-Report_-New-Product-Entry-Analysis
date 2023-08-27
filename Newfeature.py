4bd44654a8efcaeca7e5621be0be09e4cb36d570
Roshanwrites <90674961+Roshanwrites@users.noreply.github.com>
2023-08-27 17:58:43 +0530

    Newfeature is added to test it
    just a testing process it is

---
+++ Newfeature.py
@@ -0,0 +1,14 @@
+def calculate_rectangle_area(length, width):
+    """Calculate the area of a rectangle."""
+    area = length * width
+    return area
+
+# Input values for length and width
+rectangle_length = 5
+rectangle_width = 3
+
+# Call the new function and store the result
+area_of_rectangle = calculate_rectangle_area(rectangle_length, rectangle_width)
+
+# Display the calculated area
+print("The area of the rectangle is:", area_of_rectangle)
